c4b8f445ed7a99cecaa3462e310b728bff0cb1de
Declan <declankolakowski@gmail.com>
2020-04-02 10:27:23 +0000

    refactored triangle area calculation to use vector2d

--- lib/tri_module.rb
+++ lib/tri_module.rb
@@ -10,7 +10,8 @@ module Tri
   # calculates area of arbitrary triangle using:
   # (ax(by - cy) + bx(cy - ay) + cx(ay - by)) / 2
   def self.area(p1, p2, p3)
-    (((p1[0]*(p2[1]-p3[1])) + (p2[0]*(p3[1]-p1[1])) + (p3[0]*(p1[1]-p2[1])))/2).abs
+    (((p1.x*(p2.y-p3.y)) + (p2.x*(p3.y-p1.y)) + (p3.x*(p1.y-p2.y)))/2).abs
+    #(((p1[0]*(p2[1]-p3[1])) + (p2[0]*(p3[1]-p1[1])) + (p3[0]*(p1[1]-p2[1])))/2).abs
   end
   
   # calculates length of side using:
